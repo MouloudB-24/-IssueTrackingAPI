e345c257813f8309d6634af3c631bb99bf23ff64
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2025-01-02 12:15:01 +0100

    add http_method_names

--- users/views.py
+++ users/views.py
@@ -8,11 +8,13 @@ from users.serializers import UserListSerializer, UserDetailSerializer
 class UserViewSet(ModelViewSet):
     serializer_class = UserListSerializer
     detail_serializer_class = UserDetailSerializer
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('id')
     permission_classes = [IsAuthenticated]
 
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+
     def get_serializer_class(self):
-        if self.action in ['retrieve', 'update', 'update']:
+        if self.action in ['retrieve', 'update', 'create']:
             return self.detail_serializer_class
         return super().get_serializer_class()
 
